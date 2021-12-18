8c24a95f4b8d99768c9f30401fe2a11340b281f6
Michael Bironneau <mbironneau@gmail.com>
2021-12-18 08:03:04 +0000

    Implement better error handling

--- backend/api/utils.go
+++ backend/api/utils.go
@@ -25,9 +25,12 @@ func getPrincipal(db *sql.DB, token string) (string, error) {
 	}
 	var res sql.NullString
 	row := db.QueryRow("SELECT TOP 1 Username FROM Security.Principal WHERE Token = @InToken", sql.Named("InToken", token))
-	if err := row.Scan(&res); err != nil {
+	if err := row.Scan(&res); err != nil && err != sql.ErrNoRows {
 		log.Printf("Error retrieving principal from credentials: %v", err)
 		return "", Err500
+	} else if err == sql.ErrNoRows {
+		log.Printf("Invalid token")
+		return "", Err401
 	}
 	if !res.Valid {
 		log.Printf("Invalid token: %v", token)
